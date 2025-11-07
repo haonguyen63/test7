52bff3d900f5549caa2f42ca9ce2f05a8fa42162
haonguyen63 <101074056+haonguyen63@users.noreply.github.com>
2025-11-07 08:32:27 +0700

    Update route.ts

--- app/api/export/route.ts
+++ app/api/export/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server"
-import { prisma } from "@/lib/prisma"
+import prisma from "@/lib/prisma"
 import Papa from "papaparse"
 import { authOptions } from "../auth/[...nextauth]/route"
 import { getServerSession } from "next-auth"
