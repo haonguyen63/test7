d9c8c895c6107fbd8fbdab8e010ba10d12528bb0
haonguyen63 <101074056+haonguyen63@users.noreply.github.com>
2025-11-07 08:15:15 +0700

    Delete loyalty-render-uiv7 directory

--- loyalty-render-uiv7/pages/api/auth.js
+++
@@ -1,8 +0,0 @@
-
-export default function handler(req, res) {
-  if (req.method === 'POST') {
-    res.status(200).json({ message: 'Login route' });
-  } else {
-    res.status(405).json({ message: 'Method Not Allowed' });
-  }
-}
