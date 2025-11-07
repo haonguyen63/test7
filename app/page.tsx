c65492c9f3139847d940e5eb498433f5f807593e
haonguyen63 <101074056+haonguyen63@users.noreply.github.com>
2025-11-07 08:20:49 +0700

    Add files via upload

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center p-24">
+      <h1 className="text-4xl font-bold">Điểm tích lũy</h1>
+      <p className="mt-3 text-2xl">Chào mừng đến hệ thống tích điểm!</p>
+    </main>
+  )
+}
